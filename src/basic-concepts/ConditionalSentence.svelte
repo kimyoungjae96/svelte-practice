c9e2a63f36ccce64da7848a638387f441236e28a
kimyoungjae <sskim5421@gmail.com>
2021-09-25 18:44:16 +0900

    Add conditional sentence

---
+++ src/basic-concepts/ConditionalSentence.svelte
@@ -0,0 +1,12 @@
+<script>
+    let name = 'youngjae'
+    let toggle = false
+
+</script>
+
+<button on:click={()=>{toggle = !toggle}}>toggle</button>
+{#if toggle}
+    <h1>toggle</h1>
+{:else}
+    <h1>else sentence</h1>
+{/if}
