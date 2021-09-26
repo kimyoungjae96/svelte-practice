97e0c30807c26d6572ad6f9a8dc1bdc2abfb7eed
kimyoungjae <sskim5421@gmail.com>
2021-09-26 18:38:49 +0900

    Add Logo

--- movie-app/src/components/Header.svelte
+++ movie-app/src/components/Header.svelte
@@ -7,10 +7,11 @@
 <script>
   import { link } from 'svelte-spa-router';
   import active from 'svelte-spa-router/active';
+  import Logo from '~/components/Logo';
 </script>
 
 <header>
-  헤더
+  <Logo />
   <a use:link use:active href="/">Home</a>
   <a use:link use:active="{'/about'}" href="/about">About</a>
 </header>
---
+++ movie-app/src/components/Logo.svelte
@@ -0,0 +1,20 @@
+<style lang="scss">
+  a {
+    display: block;
+    font-family: 'Oswald', sans-serif;
+    font-size: 20px;
+    color: rgba(#fff, 0.5);
+    text-decoration: none;
+    span {
+      color: #fdc000;
+    }
+  }
+</style>
+
+<script>
+  import { link } from 'svelte-spa-router';
+</script>
+
+<a use:link href="/">
+  <span>OMDbAPI</span>.com
+</a>
