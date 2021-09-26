c85634e3efad636c46616c7a65d6c6c5fd753bf0
kimyoungjae <sskim5421@gmail.com>
2021-09-26 18:14:20 +0900

    Change prettier setting

---
+++ movie-app/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: true,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'avoid',
+};
--- movie-app/src/components/Header.svelte
+++ movie-app/src/components/Header.svelte
@@ -1,16 +1,16 @@
+<style lang="scss">
+  header :global(a.active) {
+    color: red;
+  }
+</style>
+
 <script>
-    import { link } from 'svelte-spa-router'
-    import active from 'svelte-spa-router/active'
+  import { link } from 'svelte-spa-router';
+  import active from 'svelte-spa-router/active';
 </script>
 
 <header>
-    헤더
-    <a use:link use:active href="/">Home</a>
-    <a use:link use:active={'/about'} href="/about">About</a>
+  헤더
+  <a use:link use:active href="/">Home</a>
+  <a use:link use:active="{'/about'}" href="/about">About</a>
 </header>
-
-<style lang="scss">
-    header :global(a.active){
-        color:red
-    }
-</style>
