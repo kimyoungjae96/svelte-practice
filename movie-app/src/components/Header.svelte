81516ee3deb5742f84f9dec310712807aaf98304
kimyoungjae <sskim5421@gmail.com>
2021-09-26 17:59:57 +0900

    Add routes on Header

--- movie-app/src/components/Header.svelte
+++ movie-app/src/components/Header.svelte
@@ -1,3 +1,16 @@
+<script>
+    import { link } from 'svelte-spa-router'
+    import active from 'svelte-spa-router/active'
+</script>
+
 <header>
     헤더
+    <a use:link use:active href="/">Home</a>
+    <a use:link use:active={'/about'} href="/about">About</a>
 </header>
+
+<style lang="scss">
+    header :global(a.active){
+        color:red
+    }
+</style>
