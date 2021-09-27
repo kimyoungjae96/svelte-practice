1fc835c9e60b9fb61ae951fb9581e7ea782f4bb6
kimyoungjae <sskim5421@gmail.com>
2021-09-27 21:24:15 +0900

    Add Footer

--- movie-app/src/App.svelte
+++ movie-app/src/App.svelte
@@ -2,7 +2,9 @@
   import Router from 'svelte-spa-router';
   import Header from '~/components/Header.svelte';
   import routes from '~/routes';
+  import Footer from './components/Footer.svelte';
 </script>
 
 <Header />
 <Router routes="{routes}" />
+<Footer />
---
+++ movie-app/src/components/Footer.svelte
@@ -0,0 +1,28 @@
+<style lang="scss">
+  footer {
+    padding: 70px;
+    text-align: center;
+    a {
+      color: $color-white-10;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+</style>
+
+<footer>
+  <div>
+    <a
+      href="https://github.com/kimyoungjae96/svelte-practice/tree/master/movie-app"
+      >GitHub Repo</a
+    >
+  </div>
+  <div>
+    <a href="https://kimyoungjae96.github.io/portfolio/">
+      @{new Date().getFullYear()}
+      YoungJae
+    </a>
+  </div>
+</footer>
