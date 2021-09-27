34cb0b5ff4c9529ed814e657024c49d64d6ef971
kimyoungjae <sskim5421@gmail.com>
2021-09-27 21:14:12 +0900

    Apply css to Header

--- movie-app/src/components/Header.svelte
+++ movie-app/src/components/Header.svelte
@@ -1,6 +1,54 @@
 <style lang="scss">
+  header {
+    padding: 20px 40px;
+    background-color: $color-black-90;
+    position: sticky;
+    top: 0;
+    z-index: 9;
+    display: flex;
+    align-items: flex-end;
+    nav {
+      margin-left: 40px;
+      ul {
+        display: flex;
+        li {
+          margin-left: 10px;
+          &:first-child {
+            margin-left: 0;
+          }
+          a {
+            font-size: 14px;
+            font-weight: 700;
+            color: $color-white-50;
+            text-decoration: none;
+          }
+        }
+      }
+    }
+    .user {
+      width: 40px;
+      height: 40px;
+      padding: 6px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      background-color: $color-area;
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      margin: auto;
+      right: 40px;
+      transition: 0.4s;
+      &:hover {
+        background-color: lighten($color-area, 20%);
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
   header :global(a.active) {
-    color: red;
+    color: $color-primary !important;
   }
 </style>
 
@@ -39,4 +87,7 @@
       {/each}
     </ul>
   </nav>
+  <div class="user">
+    <img src="/assets/svelte.png" alt="user" />
+  </div>
 </header>
