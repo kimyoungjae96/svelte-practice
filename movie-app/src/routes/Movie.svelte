c1f72defff3cd4213045c41491ef4890891d86d1
kimyoungjae <sskim5421@gmail.com>
2021-10-03 16:56:59 +0900

    Add skeleton on Movie Screen

--- movie-app/src/routes/Movie.svelte
+++ movie-app/src/routes/Movie.svelte
@@ -1,6 +1,66 @@
+<style lang="scss">
+  .skeleton-loader {
+    display: flex;
+    .skeletons {
+      flex: 1;
+      .skeleton {
+        width: 100%;
+        height: 100px;
+        background-color: $color-area;
+        border-radius: 8px;
+        margin-top: 20px;
+
+        &:first-child {
+          margin-top: 0;
+        }
+
+        &.title {
+          height: 100px;
+        }
+
+        &.specs {
+          width: 60px;
+          height: 30px;
+        }
+
+        &.plot {
+          height: 300px;
+        }
+
+        &.etc {
+          width: 40%;
+          height: 50px;
+        }
+      }
+    }
+  }
+  .poster {
+    flex-shrink: 0;
+    width: 500px;
+    height: 500px * 3/2;
+    border-radius: 10px;
+    margin-right: 70px;
+    background-color: $color-area;
+  }
+</style>
+
 <script>
+  import Loader from '../components/Loader.svelte';
+
   export let params = {};
 </script>
 
-<h1>Movie!</h1>
-<h2>{params.id}</h2>
+<div class="container">
+  <div class="skeleton-loader">
+    <div class="poster"></div>
+    <div class="skeletons">
+      <div class="skeleton title"></div>
+      <div class="skeleton specs"></div>
+      <div class="skeleton plot"></div>
+      <div class="skeleton etc"></div>
+      <div class="skeleton etc"></div>
+      <div class="skeleton etc"></div>
+    </div>
+    <Loader absolute />
+  </div>
+</div>
