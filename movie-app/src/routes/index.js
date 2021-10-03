9a3c5447ea0793ebb8c0bbca58dda4e43ec29337
kimyoungjae <sskim5421@gmail.com>
2021-10-03 18:34:18 +0900

    Add not found page

--- movie-app/src/routes/index.js
+++ movie-app/src/routes/index.js
@@ -1,9 +1,11 @@
 import Home from './Home.svelte';
 import About from './About.svelte';
 import Movie from './Movie.svelte';
+import NotFound from './NotFound.svelte';
 
 export default {
   '/': Home,
   '/about': About,
   '/movie/:id': Movie,
+  '*': NotFound,
 };
